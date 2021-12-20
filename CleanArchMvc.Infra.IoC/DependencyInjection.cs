2d101e3016d302687f04fa08c4ec66467bfcd88b
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-20 10:54:54 -0300

    Teste - registrando os servicos e criando o Controller e View para Category

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using CleanArchMvc.Infra.Data.Context;
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
 using CleanArchMvd.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +27,11 @@ namespace CleanArchMvc.Infra.IoC
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             return services;
         }

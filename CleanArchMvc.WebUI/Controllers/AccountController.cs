e888fa074b4dfb7b72a5889240545226c1bf913a
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-20 23:06:26 -0300

    Implementando o Login,Register e Logout em AccountController

--- CleanArchMvc.WebUI/Controllers/AccountController.cs
+++ CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using CleanArchMvc.Domain.Account;
+using CleanArchMvc.WebUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,57 @@ namespace CleanArchMvc.WebUI.Controllers
         public AccountController(IAuthenticate authenticate)
         {
             _authenticate = authenticate;
+        }
+
+        [HttpGet]
+        public IActionResult Login (string returnUrl)
+        {
+            return View(new LoginViewModel() { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Login (LoginViewModel model)
+        {
+            var result = await _authenticate.Authenticate(model.Email, model.Password);
+            if (result)
+            {
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(model.ReturnUrl);
+            } else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                return View(model);
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var result = await _authenticate.RegisterUser(model.Email, model.Password);
+            if (result)
+            {
+                return Redirect("/");
+            } else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid register attempt");
+                return View(model);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _authenticate.Logout();
+            return Redirect("/Account/Login");
         }
     }
 }

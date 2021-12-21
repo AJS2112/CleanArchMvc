2ccd80c1e814fde4a3b93c45421544ac4a607f14
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-21 05:48:07 -0300

    API HttpPost criando uma nova categoria

--- CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -30,7 +30,7 @@ namespace CleanArchMvc.API.Controllers
             return Ok(categories);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="GetCategory")]
         public async Task<ActionResult<CategoryDTO>> Get(int id)
         {
             var category = await _categoryService.GetById(id);
@@ -39,6 +39,16 @@ namespace CleanArchMvc.API.Controllers
                 return NotFound("Category not found");
             }
             return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO)
+        {
+            if (categoryDTO == null)
+                return BadRequest("Invalid Data");
+
+            await _categoryService.Add(categoryDTO);
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDTO.Id }, categoryDTO);
         }
     }
 }
